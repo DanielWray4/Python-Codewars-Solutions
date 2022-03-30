bb4d0427dd61990abcfb75f81d2775406cc58152
Daniel Wray <daniel_wray@hotmail.com>
2022-03-30 09:18:17 +0100

    Update Multiply.py

--- Multiply.py
+++ Multiply.py
@@ -1,5 +1,9 @@
+#Receives integer input from the user and multiplies the quantities. 
+#@DanielWray4
+
 def multiply(x,y):
     return x*y
     num1 = int(input())
     num2 = int(input())
     print(multiply)
+>>>>>>> af6d95993411d1faeb48cf5180d1a7cb3bc34a32
